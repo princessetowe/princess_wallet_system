655250c0633714a07a4e943beb66a062b58d1a2a
princessetowe <princessetowe@gmail.com>
2025-11-03 12:14:41 +0100

    c

--- WALLET/userreg/urls.py
+++ WALLET/userreg/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('logout/', LogoutAPIView.as_view()),
     path('kyc/upload/', KYCUploadView.as_view(), name='kyc-upload'),
     path('kyc/verify/<int:pk>/', KYCVerifyView.as_view(), name='kyc-verify'),
-    path('wallet/upgrade/', WalletUpgradeAPIView.as_view(), name='wallet-upgrade'),
     path('adminprofile/', AdminProfileView.as_view(), name='admin-profile'),
     path('createadmin/', AdminProfileCreateView.as_view(), name="admin-profile-create"),
     path('verify-email/<str:token>/', VerifyEmailAPIView.as_view()),
